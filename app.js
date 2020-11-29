72acfc40f2248e22b7dc5bba3cb731c32109939b
nikki <rk_787@hotmail.co.uk>
2020-11-29 18:05:03 +0000

    read me added

--- app.js
+++ app.js
@@ -10,32 +10,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 var teamMembers = [];
-//Team memnbers
+
 function startCreatingTeam() {
-  inquirer //Create a Manager
+  inquirer 
     .prompt([
       {
         name: "name",
@@ -65,7 +43,7 @@ function startCreatingTeam() {
     });
 }
 function createThisManagersTeam() {
-  inquirer //Add Members  should i add type: "list" or leave as "checkbox?",
+  inquirer 
     .prompt([
       {
         type: "list",
@@ -76,7 +54,7 @@ function createThisManagersTeam() {
     ])
     .then((employeeType) => {
       if (employeeType.typeMember === "Engineer") {
-        inquirer //Add Engineer
+        inquirer 
           .prompt([
             {
               name: "name",
@@ -105,7 +83,7 @@ function createThisManagersTeam() {
           });
       } else if (employeeType.typeMember === "Intern") {
         inquirer.prompt([
-          //Create a Intern
+      
           {
             type: "input",
             name: "name",
